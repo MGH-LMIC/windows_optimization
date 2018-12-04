7ef1287363d2ee440b094fe5db460b157f5d2e1d
Paul Myeongchan Kim <kmc5500@naver.com>
2018-12-04 14:48:04 -0500

    Add initializer

--- WindowsOpt.py
+++ WindowsOpt.py
@@ -4,6 +4,7 @@ from keras import layers
 from keras import backend as K
 from keras import regularizers
 from keras.preprocessing import image
+from keras.initializers import Constant
 from keras.layers import Input, Dropout, Lambda, MaxPooling1D, MaxPooling2D, Dense, Activation, Concatenate, GlobalAveragePooling2D, GlobalAveragePooling1D, AveragePooling2D, UpSampling2D, ZeroPadding2D, Softmax, Layer
 from keras.layers.convolutional import Conv1D, Conv2D
 from keras.layers.normalization import BatchNormalization
@@ -33,24 +34,14 @@ def WindowOptimizer(act_window="sigmoid", upbound_window=255, nch_window=1, init
         Same shape as input.
     '''
 
-    ## Check parameter integrity.
-    if nch_window == 1:
-        assert(type(init_windows) == str)
-    else:
-        assert(type(init_windows) == list and len(init_windows) == nch_window)
-
     ## TODO: customizable layer name
     wc_name = 'window_conv'
     wa_name = 'window_act'
 
-    ## Set convolution layer
-    conv_layer = Conv2D(filters=nch_window, kernel_size=(1, 1), strides=(1, 1), padding="same", name=wc_name, **kwargs)
+    conv_layer = WinOptConv(1, conv_layer_name=wc_name, init_windows=init_windows, act_window=act_window, upbound_window=upbound_window)
 
     ## Set activation layer
-    act_layer = WinOptActivation(act_window=act_window, upbound_window=upbound_window, name=wa_name)
-
-    ## Initialize convolution layer
-    initialize_layer(conv_layer, act_window=act_window, window_names=init_windows, upbound_value=upbound_window)
+    act_layer = WinOptActivation(act_window=act_window, upbound_window=upbound_window, act_layer_name=wa_name)
 
     ## Return layer funcion
     def window_func(x):
@@ -61,7 +52,32 @@ def WindowOptimizer(act_window="sigmoid", upbound_window=255, nch_window=1, init
     return window_func
 
 
-def WinOptActivation(act_window, upbound_window, name):
+def WinOptConv(nch_window, conv_layer_name, init_windows, act_window, upbound_window, **kwargs):
+    ## Check parameter integrity.
+    if nch_window == 1:
+        assert (type(init_windows) == str)
+    else:
+        assert (type(init_windows) == list and len(init_windows) == nch_window)
+
+    ## TODO : Make function for when winopt_conv layer have mutiple channels. Now it's only support for one channel.
+
+    w_new, b_new = get_initial_parameter(window_settings, window_name=init_windows, act_window=act_window,  upbound_value=upbound_window)
+
+    conv_layer = Conv2D(filters=nch_window, kernel_size=(1, 1), strides=(1, 1), padding="same", name=conv_layer_name,
+                        kernel_initializer=Constant(w_new) , bias_initializer=Constant(b_new),
+                        **kwargs)
+    return conv_layer
+
+
+def get_initial_parameter(window_settings, window_name, act_window, upbound_value):
+    ## Get window settings from dictionay
+    wl, ww = window_settings[window_name]
+    ## Set convolution layer
+    w_new, b_new = get_init_value(wl, ww, act_window, upbound_value)
+    return w_new, b_new
+
+
+def WinOptActivation(act_window, upbound_window, act_layer_name):
     def upbound_relu(x):
         return K.minimum(K.maximum(x,0),upbound_window)
 
@@ -69,9 +85,9 @@ def WinOptActivation(act_window, upbound_window, name):
         return upbound_window*K.sigmoid(x)
 
     if act_window == "relu":
-        act_layer = Activation(upbound_relu, name=name)
+        act_layer = Activation(upbound_relu, name=act_layer_name)
     elif act_window == "sigmoid":
-        act_layer = Activation(upbound_sigmoid, name=name)
+        act_layer = Activation(upbound_sigmoid, name=act_layer_name)
     else:
         ## Todo: make a proper exception for here
         raise Exception()
@@ -79,7 +95,18 @@ def WinOptActivation(act_window, upbound_window, name):
     return act_layer
 
 
-def initialize_layer(layer, act_window, window_names='abdomen', upbound_value=255.0):
+def get_init_value(wl, ww, act_window, upbound_value):
+    if act_window == 'sigmoid':
+        w_new, b_new = get_init_conv_params_sigmoid(wl, ww, upbound_value=upbound_value)
+    elif act_window == 'relu':
+        w_new, b_new = get_init_conv_params_relu(wl, ww, upbound_value=upbound_value)
+    else:
+        ## TODO : make a proper exception
+        raise Exception()
+    return w_new, b_new
+
+
+def load_weight_layer(layer, act_window, window_names='abdomen', upbound_value=255.0):
     '''
     :param layer: 1x1 conv layer to initialize
     :param act_window: str. sigmoid or relu
@@ -88,19 +115,10 @@ def initialize_layer(layer, act_window, window_names='abdomen', upbound_value=25
     :return:
     '''
 
-    ## TODO : Make function for when conv layer have mutiple channels. Now it's only support for one channel.
-
     ## Get window settings from dictionay
     wl, ww = window_settings[window_names]
 
-    if act_window == 'sigmoid':
-        w_new, b_new = get_init_conv_params_sigmoid(wl, ww, upbound_value=upbound_value)
-    elif act_window == 'relu':
-        w_new, b_new = get_init_conv_params_relu(wl, ww, upbound_value=upbound_value)
-    else:
-        ## TODO : make a proper exception
-        raise Exception()
-
+    w_new, b_new = get_init_value(wl, ww, act_window, upbound_value)
     w_conv_ori, b_conv_ori = layer.get_weights()
     w_conv_new = np.zeros_like(w_conv_ori)
     w_conv_new[0, 0, 0, :] = w_new * np.ones(w_conv_ori.shape[-1], dtype=w_conv_ori.dtype)
@@ -113,3 +131,29 @@ def initialize_layer(layer, act_window, window_names='abdomen', upbound_value=25
 def load_pretrained_weights(model, conv_layer_name='window_conv', classifier='ICH'):
     ## TODO : Load pretrained weights of ICH and Stone classifier.
     pass
+
+
+if __name__ == "__main__":
+    # x = load_example_dicom() # They should be 2d-HU values matrix
+    input_shape = (512, 512, 1)
+    hu_inputs = keras.layers.Input(input_shape)
+    ## TODO: make test code for win opt
+
+    ## An example of a single-layer model of window optimization
+    x = WindowOptimizer(nch_window=1, act_window="sigmoid", upbound_window=255, init_windows='abdomen')(hu_inputs)
+
+    ## ... add some your layer here
+    x = Conv2D(32, (3, 3), activation=None, padding="same", name="conv1")(x)
+    x = Activation("relu", name="conv1_relu")(x)
+    x = MaxPooling2D((7, 7), strides=(3, 3), name="pool1")(x)
+    x = Conv2D(256, (3, 3), activation=None, padding="same", name="conv2")(x)
+    x = Activation("relu", name="conv2_relu")(x)
+    x = MaxPooling2D((7, 7), strides=(3, 3), name="pool2")(x)
+    x = GlobalAveragePooling2D(name="gap")(x)
+    outputs = Dense(1, activation='sigmoid')(x)
+
+    model = Model(inputs=hu_inputs, outputs=outputs, name="main_model")
+    optimizer = SGD(lr=0.0001, decay=0, momentum=0.9, nesterov=True)
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=["accuracy"])
+    model.summary()
+
